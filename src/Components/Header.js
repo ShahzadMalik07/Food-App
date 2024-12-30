6c7757856506908bdaac8a782ca455899784490e
ShahzadMalik07 <shahzadmalik425@gmail.com>
2024-12-30 23:51:59 +0530

    header ui updated

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,19 +8,19 @@ import { useSelector } from "react-redux"
 
 
 
-const Header = ()=>{
+const Header = () => {
     const onlineStatus = useOnlineStatus()
-    const[name, setName] = useState("Login")
+    const [name, setName] = useState("Login")
     // console.log("header")
     // useEffect(()=>{console.log("useefeect")},[name])
 
     const contextdata = useContext(UserContext)
 
-    
-    const cartItems = useSelector((store)=>store.cart.Item)
+
+    const cartItems = useSelector((store) => store.cart.Item)
     // console.log(cartItems)
-    
- 
+
+
 
     return (
         <div className="header">
@@ -29,14 +29,22 @@ const Header = ()=>{
             </div>
             <div className="nav-items">
                 <ul>
-                    <li>onlineStatus:{onlineStatus?"✅":"❌"}</li>
+                    <li>{onlineStatus ? <div className=" w-[100px] h-[75px] flex items-center justify-end bg-red-300  ">
+                        <h2 className="relative group h-[14px] w-[14px] mr-2 bg-green-400 rounded-full group">
+                            <div className="absolute group-hover:block hidden mr-3 border text-sm bg-white">Online</div>
+                        </h2>
+
+                    </div>
+                        : <div className="h-4 w-4 bg-red-500 rounded-full"></div>}</li>
+
+
                     <Link className="px-3 py-1 bg-orange-500 rounded-md text-white" to="/" >Home</Link>
                     <Link className="px-3 py-1 bg-orange-500 rounded-md text-white" to="/grocery" >Grocery</Link>
-                   
+
                     <li className="bg-orange-500 rounded-md text-white">Cart - {cartItems.length}</li>
-                    <button className="px-3 py-1 bg-orange-500 rounded-md text-white" onClick={()=>{name=="Login"?setName("Logout"):setName("Login")}}>{name}</button>
-                    <h6 className="px-3 py-1 bg-green-400 rounded-md text-white">{contextdata.logged}</h6>
-                 
+                    <button className="px-3 py-1 bg-orange-500 rounded-md text-white" onClick={() => { name == "Login" ? setName("Logout") : setName("Login") }}>{name}</button>
+                    <h6 className="px-3 py-1 bg-blue-700 rounded-md text-white">{contextdata.logged}</h6>
+
                 </ul>
             </div>
 
